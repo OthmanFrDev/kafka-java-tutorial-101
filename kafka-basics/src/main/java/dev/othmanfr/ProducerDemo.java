d54afa42b7c1f7ef37e8e6e765f7178a8cd28ac3
unknown <perso@OFROUKH1.emea.cegedim.grp>
2023-02-24 23:56:19 +0100

    Sending message asynchrounsly and synchrounsly

---
+++ kafka-basics/src/main/java/dev/othmanfr/ProducerDemo.java
@@ -0,0 +1,58 @@
+package dev.othmanfr;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemo {
+    private static Logger log= LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+    public static void main(String[] args) {
+        log.info("Start sending event");
+        Properties properties=new Properties();
+        // broker properties
+        properties.setProperty("bootstrap.servers","127.0.0.1:9091");
+        // producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        // create a producer
+        KafkaProducer<String,String> producer=new KafkaProducer<>(properties);
+        // create a producer record
+        ProducerRecord<String,String> producerRecord=new ProducerRecord<>("demo","hello world");
+        // send data
+        asyncProduce(producer,producerRecord);
+        // tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+        // flush and close the producer
+        producer.close();
+        log.info("Finish sending event");
+    }
+    private static void asyncProduce(KafkaProducer producer,ProducerRecord producerRecord){
+        try {
+            producer.send(producerRecord).get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void syncProduce(KafkaProducer producer,ProducerRecord producerRecord){
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                if(exception!=null){
+                    throw new RuntimeException(exception.getMessage());
+                }
+                else{
+                    log.info("Event sended successfully to topic {}",metadata.topic());
+                }
+            }
+        });
+    }
+}
